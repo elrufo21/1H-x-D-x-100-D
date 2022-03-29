8678b09805d1fe40536eb6a689b2ff063b413dde
elrufo21 <neburtnt@gmail.com>
2022-03-29 17:17:12 -0500

    v1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <!--Hora de inicio 17:13-->
+    <h1>rasdasdasdasd</h1>
+
+</body>
+</html>
