760a3aabf55e30bd1a6212b3dacdeaa76b68e1fa
elrufo21 <neburtnt@gmail.com>
2022-03-29 18:30:45 -0500

    Dia 1 completado

--- index.php
+++ index.php
@@ -1,14 +1,65 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="/Estilos/estiloIndex.css">
+    <link rel="stylesheet" href="Estilos/estiloIndex.css">
 </head>
+
 <body>
     <!--Hora de inicio 17:13-->
-    <h1>rasdasdasdasd</h1>
-
+    <div id="principal">
+        <nav>
+            <header>
+                <div class="menu">
+                    <ul class="menu-horizontal">
+                        <li>
+                            <a href="#">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="">Ideas!!</a>
+                        </li>
+                        <li>
+                            <a href="">MasIdeas</a>
+                            <ul class="menu-vertical">
+                                <li><a href="">idea1</a></li>
+                                <li><a href="">idea2</a></li>
+                                <li><a href="">idea3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">MasIdeas2</a>
+                            <ul class="menu-vertical">
+                                <li><a href="">idea1</a></li>
+                                <li><a href="">idea2</a></li>
+                                <li><a href="">idea3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </nav>
+        <main>
+            <div class="contenedor-img">
+                <img src="Estilos/tesla.jpg">
+            </div>
+            <div class="contenedor-txt">
+                <h2 class="titulo">
+                    Aqui ira un texto que puede <br>
+                    cambiar <span class="typed"></span>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste maiores natus iure, quidem aut id
+                         corporis perferendis corrupti eveniet, cupiditate eligendi, dolores qui itaque. Dignissimos molestiae 
+                         officiis cum repellendus itaque.</p>
+                </h2>
+            </div>
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12" ></script>
+    <script src="javaScript/main.js"></script>
 </body>
+
 </html>
